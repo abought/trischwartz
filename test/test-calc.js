6341cc29595b61fc9101c9a06a24030604521bac
Andy Boughton <abought@gmail.com>
2017-01-28 17:36:46 -0500

    Fix unit test and remove use of es6

--- test/test-calc.js
+++ test/test-calc.js
@@ -1,10 +1,13 @@
 // TODO: Test calculation functions
 
 // - Coord gen
-describe('testCoords', function() {
+describe('generateCoords', function() {
     it('generates an equilateral triangle', function() {
         const res = generateCoords(60, 60, 60);
-        assert.equal(res,
-        [[0,0], [0,1], [0, 0.867]]);
+
+        // TODO: Floating point equality, can be improved
+        assert.deepEqual(res, [
+            [0,0], [1,0], [0.5000000000000001, 0.8660254037844386]
+        ]);
     });
 });
