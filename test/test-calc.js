0c8295a2825d79436be632daa675258c12b63b0f
Andy Boughton <abought@gmail.com>
2017-01-29 00:09:38 -0500

    Improve float handling and more coord tests

--- test/test-calc.js
+++ test/test-calc.js
@@ -1,12 +1,47 @@
 // Tests for coordinate generation
 chai.config.truncateThreshold = 0;
-describe('generateCoords', function() {
-    it('generates an equilateral triangle', function() {
-        const res = generateCoords(60, 60, 60);
 
-        // TODO: Floating point equality, can be improved
+function roundHelper(coords) {
+    // Round the results to ensure deep comparisons of floating point numbers
+    if (typeof coords === "number") {
+        return coords.toFixed(3);
+    } else {
+        return coords.map(roundHelper);
+    }
+}
+
+describe('generateCoords', function() {
+    // Generate coordinates and round results as appropriate
+    function coordHelper(ang1, ang2, ang3) {
+        return roundHelper(generateCoords(ang1, ang2, ang3));
+    }
+
+    it('generates an equilateral triangle', function() {
+        const res = coordHelper(60, 60, 60);
+
         assert.deepEqual(res, [
-            [0,0], [1,0], [0.5000000000000001, 0.8660254037844386]
+            ['0.000', '0.000'], ['1.000', '0.000'], ['0.500', '0.866']
+        ]);
+    });
+
+    it('generates a right triangle', function() {
+        const res = coordHelper(90, 45, 45);
+        assert.deepEqual(res, [
+            ['0.000', '0.000'], ['1.000', '0.000' ], ['0.000', '1.000']
+        ]);
+    });
+
+    it('generates an isosceles triangle', function() {
+        const res = coordHelper(70, 70, 40);
+        assert.deepEqual(res, [
+            ['0.000', '0.000'], ['1.000', '0.000' ], ['0.500', '1.374']
+        ]);
+    });
+
+    it('generates an obtuse triangle', function() {
+        const res = coordHelper(120, 30, 30);
+        assert.deepEqual(res, [
+            ['0.000', '0.000'], ['1.000', '0.000' ], ['-0.500', '0.866']
         ]);
     });
 });
