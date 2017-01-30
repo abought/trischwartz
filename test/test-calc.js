2423fbb20a7ef4de22d0e44a7c8b375466142380
Andy Boughton <abought@gmail.com>
2017-01-29 23:08:10 -0500

    Add genAngles tests + sinon

--- test/test-calc.js
+++ test/test-calc.js
@@ -18,16 +18,22 @@ describe('generateCoords', function() {
 
     it('generates an equilateral triangle', function() {
         const res = coordHelper(60, 60, 60);
-
         assert.deepEqual(res, [
             ['0.000', '0.000'], ['1.000', '0.000'], ['0.500', '0.866']
         ]);
     });
 
-    it('generates a right triangle', function() {
+    it('generates a right triangle facing right', function() {
         const res = coordHelper(90, 45, 45);
         assert.deepEqual(res, [
             ['0.000', '0.000'], ['1.000', '0.000' ], ['0.000', '1.000']
+        ]);
+    });
+
+    it('generates a right triangle facing left', function() {
+        const res = coordHelper(45, 90, 45);
+        assert.deepEqual(res, [
+            ['0.000', '0.000'], ['1.000', '0.000' ], ['1.000', '1.000']
         ]);
     });
 
@@ -48,6 +54,46 @@ describe('generateCoords', function() {
 
 
 describe('generateAngles', function() {
+    it('generates three angles given none', function() {
+        const res = generateAngles();
+        assert.lengthOf(res, 3);
+        assert.equal(res.reduce(function(a,b) {return a+b}),
+            180.0);
+    });
+
+    it('generates two angles + uses the one given', function() {
+        const res = generateAngles(30);
+        assert.lengthOf(res, 3);
+
+        assert.equal(res[0], 30);
+        assert.equal(res.reduce(function(a,b) {return a+b}),
+            180.0);
+    });
+
+    it('No angle can be less than the minimum', function() {
+        sinon.stub(Math, 'random', function () {return 0;});
+        const res = generateAngles(30, 25);
+        Math.random.restore();
+
+        assert.lengthOf(res, 3);
+
+        assert.equal(res[0], 30);
+        assert.equal(res[1], 25);
+        assert.equal(res.reduce(function(a,b) {return a+b}),
+            180.0);
+    });
+
+    it('A big angle leaves room for the others', function() {
+        var minAngle = 25;
+        sinon.stub(Math, 'random', function () {return 0.99999;});
+        const res = generateAngles(null, minAngle);
+        Math.random.restore();
+
+        assert.lengthOf(res, 3);
+        assert.isTrue(res.every(function(item) {return item >= minAngle}));
+        assert.equal(res.reduce(function(a,b) {return a+b}),
+            180.0);
+    });
 
 });
 
